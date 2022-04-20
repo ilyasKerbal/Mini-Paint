52b46b73e8561d9ff463d53b2c812406dc1c4089
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-20 22:11:26 +0000

    Exercise: Implement touchUp()

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -95,6 +95,9 @@ class MyCanvasView(context: Context) : View(context) {
         invalidate()
     }
 
-    private fun touchUp() {}
+    private fun touchUp() {
+        // Reset the path so it doesn't get drawn again.
+        path.reset()
+    }
 
 }
