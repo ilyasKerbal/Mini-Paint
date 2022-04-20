454b05ee64fd227e224cfc106eea724a96ea3811
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-20 17:17:11 +0000

    Exercise: Set MyCanvasView as the Content View

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.android.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView : MyCanvasView = MyCanvasView(this)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+
+        //myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN (Deprecated)
+
+        WindowInsetsControllerCompat(window, myCanvasView).let {
+            it.hide(WindowInsetsCompat.Type.systemBars())
+            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
